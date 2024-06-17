a39e956977a0c4f46e5586279ceafbf51ad57e02
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-06-18 01:15:35 +0700

    buildless

--- src/mods/scripts/build/index.ts
+++
@@ -1,7 +0,0 @@
-import { readFile, writeFile } from "fs/promises"
-
-const serviceWorkerBytes = await readFile("./service_worker.js")
-const serviceWorkerHashBytes = new Uint8Array(await crypto.subtle.digest("SHA-256", serviceWorkerBytes))
-const serviceWorkerHashRawHex = Buffer.from(serviceWorkerHashBytes).toString("hex")
-
-await writeFile(`./service_worker.${serviceWorkerHashRawHex}.js`, serviceWorkerBytes)
--- src/mods/scripts/service_worker/index.ts
+++ src/mods/scripts/service_worker/index.ts
@@ -10,82 +10,44 @@ declare const self: ServiceWorkerGlobalScope
 
 async function main() {
   self.addEventListener("install", async () => {
-    /**
-     * Perform some validations on the current service worker
-     */
-
-    const currentRes = await fetch(location.href, { cache: "force-cache" })
-    const currentCacheControl = currentRes.headers.get("cache-control")
-
-    if (currentCacheControl !== "public, max-age=31536000, immutable")
-      throw new Error(`Current service worker is not properly cached`)
-
-    const currentBytes = new Uint8Array(await currentRes.arrayBuffer())
-    const currentHashBytes = new Uint8Array(await crypto.subtle.digest("SHA-256", currentBytes))
-    const currentHashRawHex = Array.from(currentHashBytes).map(b => b.toString(16).padStart(2, "0")).join("")
-
-    if (location.pathname !== `/service_worker.${currentHashRawHex}.js`)
-      throw new Error(`Current service worker has been tampered`)
-
     await self.skipWaiting()
   })
 
+  /**
+   * Check for update
+   */
   try {
-    /**
-     * Check for update
-     */
-
-    const latestRes = await fetch("/service_worker.js", { cache: "reload" })
+    const latestRes = await fetch(`/service_worker.js`, { cache: "reload" })
     const latestBytes = new Uint8Array(await latestRes.arrayBuffer())
     const latestHashBytes = new Uint8Array(await crypto.subtle.digest("SHA-256", latestBytes))
     const latestHashRawHex = Array.from(latestHashBytes).map(b => b.toString(16).padStart(2, "0")).join("")
 
-    const updateUrl = new URL(`/service_worker.${latestHashRawHex}.js`, location.href)
+    const cache = await caches.open("service_worker")
+
+    const currentReq = new Request(`/current`)
+    const currentRes = await cache.match(currentReq)
 
     /**
-     * Update available
+     * First time
      */
-    if (updateUrl.href !== location.href) {
-      /**
-       * Perform some validations on the updated service worker
-       */
+    if (currentRes == null) {
+      const pendingPath = `/service_worker.js?nonce=${latestHashRawHex}`
+      const pendingRes = new Response(pendingPath)
 
-      const updateRes = await fetch(updateUrl.href, { cache: "force-cache" })
-      const updateCacheControl = updateRes.headers.get("cache-control")
-
-      if (updateCacheControl !== "public, max-age=31536000, immutable")
-        throw new Error(`Updated service worker is not properly cached`)
-
-      const updateBytes = new Uint8Array(await updateRes.arrayBuffer())
-      const updateHashBytes = new Uint8Array(await crypto.subtle.digest("SHA-256", updateBytes))
-      const updateHashRawHex = Array.from(updateHashBytes).map(b => b.toString(16).padStart(2, "0")).join("")
-
-      if (updateUrl.pathname !== `/service_worker.${updateHashRawHex}.js`)
-        throw new Error(`Updated service worker has been tampered`)
+      await cache.put(currentReq, pendingRes)
+    } else {
+      const currentPath = await currentRes.text()
+      const pendingPath = `/service_worker.js?nonce=${latestHashRawHex}`
 
       /**
-       * Update reference
+       * Update available
        */
+      if (currentPath !== pendingPath) {
+        const pendingReq = new Request(`/pending`)
+        const pendingRes = new Response(pendingPath)
 
-      const cache = await caches.open("service_worker")
-
-      const req = new Request("/service_worker.js")
-      const res = new Response(`/service_worker.${updateHashRawHex}.js`)
-
-      await cache.put(req, res)
-
-      /**
-       * Reload to force update
-       */
-
-      await self.registration.unregister()
-
-      const clients = await self.clients.matchAll({ type: "window" })
-
-      for (const client of clients)
-        client.navigate(client.url)
-
-      return
+        await cache.put(pendingReq, pendingRes)
+      }
     }
   } catch (e: unknown) {
     console.error(e)
