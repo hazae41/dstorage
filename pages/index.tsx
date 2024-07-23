69e22d421b72a33e308b5a593d0af05e1c739c3d
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-07-23 22:29:05 +0700

    ff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   return <>
-    Welcome to DStorage! This app aims to provide a origin-agnostic storage solution for your webapp. This allows separation of the code and the storage. So your webapp can be served from any origin and still use the same storage.
+    Welcome to DStorage! This app aims to provide an origin-agnostic storage for your webapp. This allows separation of the code and the storage. So your webapp can be served from any origin and still use the same storage.
   </>
 }
