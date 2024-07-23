96ddf541457a69244cece7c495795ab49f40f327
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-07-23 22:18:49 +0700

    intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   return <>
-    Welcome to DStorage!
+    Welcome to DStorage! This app aims to provide a decentralized storage solution for your webapp. This allows separation of the code and the storage.
   </>
 }
